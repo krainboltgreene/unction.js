7e5b3baf722d87a59992dafa3fdef62dbb73117d
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-09-17 18:09:21 -0700

    mergeAll somehow omits the values?

--- source/hammer/index.js
+++ source/hammer/index.js
@@ -1,5 +1,5 @@
 import {curryN} from "ramda"
-import {mergeAll} from "ramda"
+import {merge} from "ramda"
 import {prop} from "ramda"
 import {omit} from "ramda"
 import {type} from "ramda"
@@ -9,12 +9,12 @@ export default curryN(2, function hammer (key, pairs) {
     throw new Error(`hammer only works on an Object, but the second argument was a ${type(pairs)}`)
   }
 
+  const onlyKey = prop(key, pairs)
   const withoutKey = omit(key, pairs)
-  const subvalue = prop(key, pairs)
 
-  if (type(subvalue) !== "Object") {
-    throw new Error(`The hammered property ${key} was not an Object it was a ${type(subvalue)}`)
+  if (type(onlyKey) !== "Object") {
+    throw new Error(`The hammered property ${key} was not an Object it was a ${type(onlyKey)}`)
   }
 
-  return mergeAll(withoutKey, subvalue)
+  return merge(withoutKey, onlyKey)
 })
