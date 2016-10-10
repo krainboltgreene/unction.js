64f7ebb53e3783d578d226330ca07774af0198a9
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-10-10 02:02:42 -0700

    Christ, I've got to automate this.

--- source/index.js
+++ source/index.js
@@ -1,3 +1,5 @@
 export {default as mapKeys} from "./mapKeys"
 export {default as hammer} from "./hammer"
 export {default as mergeDeep} from "./mergeDeep"
+export {default as computedProp} from "./computedProp"
+export {default as nestedObjOf} from "./nestedObjOf"
