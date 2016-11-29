aca633f36fbe7cb13fae600386eb120f8921c655
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-11-29 12:12:32 -0800

    Making sure currying works

--- source/nestedObjOf/index.js
+++ source/nestedObjOf/index.js
@@ -4,6 +4,8 @@ import {flip} from "ramda"
 import {reverse} from "ramda"
 import {objOf} from "ramda"
 
-export default curryN(2, function nestedObjOf (path, value) {
+const ARGUMENTS = 2
+
+export default curryN(ARGUMENTS, function nestedObjOf (path, value) {
   return reduce(flip(objOf), value, reverse(path))
 })
