e5162b21b36a2106057722790b06aa71cef5e2dc
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-04-24 01:07:06 -0700

    WE can use our mapValues to simplify this

--- source/mapKeys/index.js
+++ source/mapKeys/index.js
@@ -1,21 +1,13 @@
-import {fromPairs} from "ramda"
-import {map} from "ramda"
-import {adjust} from "ramda"
-import {toPairs} from "ramda"
-import {type} from "ramda"
+import {empty} from "ramda"
 
-export default function mapKeys (unction: KeyType => any): Function {
-  if (type(unction) !== "Function") {
-    throw new Error(`mapKeys only works with an Function, but the unction argument was a ${type(unction)}`)
-  }
-  const callWithKey = adjust(unction, 0)
-  const overKey = map(callWithKey)
-
+export default function mapKeys (unction: any => any): Function {
   return function mapKeysUnction (iterable: IterableType): IterableType {
-    if (type(iterable) !== "Object") {
-      throw new Error(`mapKeys only works on an Object, but the iterable argument was a ${type(iterable)}`)
-    }
+    const transformed = empty(iterable)
 
-    return fromPairs(overKey(toPairs(iterable)))
+    iterable.forEach(function mapKeysUnctionIterable (value: any, key: any) {
+      transformed[unction(key)] = value
+    })
+
+    return transformed
   }
 }
