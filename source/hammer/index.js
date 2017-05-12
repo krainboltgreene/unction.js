873dd549d34283f83b1209d4193d7282f04d740e
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-05-12 03:06:52 -0700

    Now we don't have to do custom stuff

--- source/hammer/index.js
+++ source/hammer/index.js
@@ -2,20 +2,21 @@ import {merge} from "ramda"
 import {prop} from "ramda"
 import {omit} from "ramda"
 import {type} from "ramda"
+import isIterable from "@unction/isiterable"
 
 export default function hammer (key: KeyType): Function {
   const propKey = prop(key)
   const omitKey = omit(key)
 
   return function hammerKey (iterable: IterableType): IterableType {
-    if (type(iterable) !== "Object" && type(iterable) !== "Array") {
+    if (!isIterable(iterable)) {
       throw new Error(`hammer only works on an Object or Array, but the set was a ${type(iterable)}`)
     }
 
     const only = propKey(iterable)
     const without = omitKey(iterable)
 
-    if (type(only) !== "Object" && type(only) !== "Array") {
+    if (!isIterable(only)) {
       throw new Error(`hammer only works on an Object or Array, but the subset was a ${type(iterable)}`)
     }
 
