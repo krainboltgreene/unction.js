c623646b7f0bc41bc1242224b19ff2b38514a5e3
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-07-31 14:08:04 -0700

    Adding streams and strings as iterables

--- flow-typed/index.js
+++ flow-typed/index.js
@@ -2,13 +2,14 @@ type ValueType = mixed
 type KeyType = mixed
 type ListType = Array<ValueType> | Set<ValueType> | WeakSet<ValueType>
 type RecordType = {[key: KeyType]: ValueType} | Map<KeyType, ValueType> | WeakMap<KeyType, ValueType>
-type IterableType = ListType | RecordType
+type StreamType = Stream<ValueType>
+type IterableType = ListType | RecordType | StreamType | string
 type RecordTreeType = {[key: KeyType]: ValueType | IterableType }
 type ListTreeType = Array<ValueType | IterableType>
 type TreeType = ListTreeType | RecordTreeType
 type KeyChainType = Array<KeyType>
 type UnfinishedKeyChainType = Array<KeyType | null>
 type NestedIterableType = ListTreeType | RecordTreeType
-type PredicateType = any => boolean
+type PredicateType = mixed => boolean
 type ResolvedPromisesType = ListType
 type RejectedPromisesType = ListType
