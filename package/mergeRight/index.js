cf3f71a6e0ea4d6f6542bc9b13238d421335fbbd
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-07-30 23:20:54 -0700

    Making merge right know how to deal with all iterable types

--- package/mergeRight/index.js
+++ package/mergeRight/index.js
@@ -1,40 +1,55 @@
-import {all} from "ramda"
-import couple from "@unction/couple"
-import isObject from "@unction/isobject"
-import isArray from "@unction/isarray"
-import isPresent from "@unction/ispresent"
-
-const isArrayPair = all(isArray)
-const isObjectPair = all(isObject)
+import type from "@unction/type"
+import isIterable from "@unction/isiterable"
+import xstream from "xstream"
 
 export default function mergeRight (left: IterableType): Function {
-  const pairWithLeft = couple(left)
+  if (!isIterable(left)) {
+    throw new Error("left is not an iterable")
+  }
+
+  const leftType = type(left)
 
   return function mergeRightLeft (right: IterableType): IterableType {
-    const pair = pairWithLeft(right)
-
-    if (!isPresent(left) || !isPresent(left)) {
-      throw new Error("left or right was not an iterable")
+    if (!isIterable(right)) {
+      throw new Error("right was not an iterable")
     }
 
     if (left.constructor.name !== right.constructor.name) {
       throw new Error("left and right weren't the same type")
     }
 
-    if (isArrayPair(pair)) {
-      return [
-        ...left,
-        ...right,
-      ]
-    }
+    switch (leftType) {
+      case "Array": {
+        return [
+          ...left,
+          ...right,
+        ]
+      }
+      case "Object": {
+        return {
+          ...left,
+          ...right,
+        }
+      }
+      case "Map": {
+        return new Map()
+      }
+      case "WeakMap": {
+        return new WeakMap()
+      }
+      case "Set": {
+        return new Set()
+      }
+      case "WeakSet": {
+        return new WeakSet()
+      }
+      case "Stream": {
+        return new xstream.merge(left, right)
+      }
 
-    if (isObjectPair(pair)) {
-      return {
-        ...left,
-        ...right,
+      default: {
+        throw new Error(`left wasn't an iterable type we know how to merge: ${leftType}`)
       }
     }
-
-    throw new Error("Couldn't figure out how to merge left and right")
   }
 }
