890435db6d7de2cee67287e0f42fb14b877a4a6c
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-06 04:16:59 -0700

    Using points to clear up code

--- package/partition/index.js
+++ package/partition/index.js
@@ -5,12 +5,21 @@ import reduceValues from "@unction/reducevalues"
 
 export default function partition (predicate: PredicateType): Function {
   return function partitionPredicate (iterable: IterableType): [IterableType, IterableType] {
+    const freshIterable = fresh(iterable)
+    const initalIterablePair = [
+      freshIterable,
+      freshIterable,
+    ]
+
     return reduceValues((accumulation: [IterableType, IterableType]): Function => (value: ValueType): [IterableType, IterableType] => {
+      const [consequent, alternate] = accumulation
+      const appendedValue = append(value)
+
       if (predicate(value)) {
-        return couple(append(value)(accumulation[0]))(accumulation[1])
+        return couple(appendedValue(consequent))(alternate)
       }
 
-      return couple(accumulation[0])(append(value)(accumulation[1]))
-    })(couple(fresh(iterable))(fresh(iterable)))(iterable)
+      return couple(consequent)(appendedValue(alternate))
+    })(initalIterablePair)(iterable)
   }
 }
